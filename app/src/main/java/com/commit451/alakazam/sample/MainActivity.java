c804aad6ce2bf888ad63eba33031913b93c0dae6
John Carlson <jawnnypoo@gmail.com>
2016-10-24 10:06:35 -0500

    Sample usage of Hide and show runnables

--- app/src/main/java/com/commit451/alakazam/sample/MainActivity.java
+++ app/src/main/java/com/commit451/alakazam/sample/MainActivity.java
@@ -1,7 +1,12 @@
 package com.commit451.alakazam.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.commit451.alakazam.HideRunnable;
+import com.commit451.alakazam.ShowRunnable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageView icon = (ImageView) findViewById(R.id.icon);
+        findViewById(R.id.animate).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (icon.getVisibility() == View.VISIBLE) {
+                    icon.animate().alpha(0.0f).withEndAction(new HideRunnable(icon));
+                } else {
+                    icon.animate().alpha(1.0f).withStartAction(new ShowRunnable(icon));
+                }
+            }
+        });
     }
 }
