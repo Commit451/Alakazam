5370a0f49351fd0987e01dffa8a07d9b953c14a9
John Carlson <jawnnypoo@gmail.com>
2017-02-01 16:58:48 -0600

    By default, fade in and out will set the alpha. Overridable if desired

--- alakazam-kotlin/src/main/java/com/commit451/alakazam/kotlin/alakazam.kt
+++ alakazam-kotlin/src/main/java/com/commit451/alakazam/kotlin/alakazam.kt
@@ -2,18 +2,25 @@ package com.commit451.alakazam.kotlin
 
 import android.view.View
 import android.view.ViewPropertyAnimator
-import com.commit451.alakazam.FadeInRunnable
 import com.commit451.alakazam.HideRunnable
 
 
-fun View.fadeOut(): ViewPropertyAnimator {
+fun View.fadeOut(setAlphaToOne: Boolean = true): ViewPropertyAnimator {
+    if (setAlphaToOne) {
+        alpha = 1.0f
+    }
     return animate()
             .alpha(0.0f)
             .withEndAction(HideRunnable(this))
 }
 
-fun View.fadeIn() : ViewPropertyAnimator {
+fun View.fadeIn(setAlphaToZero: Boolean = true): ViewPropertyAnimator {
+    if (setAlphaToZero) {
+        alpha = 0.0f
+    }
+    if (visibility != View.VISIBLE) {
+        visibility = View.VISIBLE
+    }
     return animate()
             .alpha(1.0f)
-            .withStartAction(FadeInRunnable(this))
 }
