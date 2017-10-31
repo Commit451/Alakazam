9a8d430a98105a3cdb76bb3ebdde365130a28f25
John <jawnnypoo@gmail.com>
2017-10-31 13:59:10 -0500

    Make Alakazam object class internal for now

--- alakazam/src/main/java/com/commit451/alakazam/Alakazam.kt
+++ alakazam/src/main/java/com/commit451/alakazam/Alakazam.kt
@@ -5,7 +5,7 @@ import android.animation.ArgbEvaluator
 /**
  * Entry point into Animation things
  */
-object Alakazam {
+internal object Alakazam {
 
     internal val argbEvaluator = ArgbEvaluator()
 }
--- alakazam/src/main/java/com/commit451/alakazam/view.kt
+++ alakazam/src/main/java/com/commit451/alakazam/view.kt
@@ -1,9 +1,46 @@
-package com.commit451.alakazam.kotlin
+package com.commit451.alakazam
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.graphics.drawable.ColorDrawable
+import android.support.annotation.ColorInt
 import android.view.View
 import android.view.ViewPropertyAnimator
-import com.commit451.alakazam.VisibilityRunnable
+import com.commit451.alakazam.Alakazam.argbEvaluator
 
+fun View.widthAnimator(currentWidth: Int, endWidth: Int): ValueAnimator {
+    val anim = ValueAnimator.ofInt(currentWidth, endWidth)
+    anim.addUpdateListener { valueAnimator ->
+        val width = valueAnimator.animatedValue as Int
+        val layoutParams = this.layoutParams
+        layoutParams.width = width
+        this.layoutParams = layoutParams
+    }
+    return anim
+}
+
+fun View.heightAnimator(currentHeight: Int, endHeight: Int): ValueAnimator {
+    val anim = ValueAnimator.ofInt(currentHeight, endHeight)
+    anim.addUpdateListener { valueAnimator ->
+        val height = valueAnimator.animatedValue as Int
+        val layoutParams = this.layoutParams
+        layoutParams.height = height
+        this.layoutParams = layoutParams
+    }
+    return anim
+}
+
+fun View.backgroundColorAnimator(@ColorInt endColor: Int): ObjectAnimator {
+    if (this.background !is ColorDrawable) {
+        throw IllegalStateException("View needs to have a background which is an instance of ColorDrawable")
+    }
+    return backgroundColorAnimator((this.background as ColorDrawable).color, endColor)
+}
+
+fun View.backgroundColorAnimator(@ColorInt startColor: Int, @ColorInt endColor: Int): ObjectAnimator {
+    return ObjectAnimator.ofObject(this, "backgroundColor", argbEvaluator,
+            startColor, endColor)
+}
 
 fun View.fadeOut(setAlphaToOne: Boolean = true, visibilityWhenComplete: Int = View.GONE): ViewPropertyAnimator {
     if (setAlphaToOne) {
@@ -11,7 +48,7 @@ fun View.fadeOut(setAlphaToOne: Boolean = true, visibilityWhenComplete: Int = Vi
     }
     return animate()
             .alpha(0.0f)
-            .withEndAction(VisibilityRunnable(this))
+            .withEndAction(VisibilityRunnable(this, visibilityWhenComplete))
 }
 
 fun View.fadeIn(setAlphaToZero: Boolean = true): ViewPropertyAnimator {
